e3059f6bfa7a8c7f914ca6f2265a36136251a5dc
tcowell <84730021+tcowell@users.noreply.github.com>
2022-09-14 10:42:57 -0400

    downloaded new version

--- in-lab-examples/puinote-lab03/puinote-start/js/app.js
+++
@@ -1,8 +0,0 @@
-console.log('Started Application.');
-
-function updateElement() {
-    console.log('Running the updateElement function!');
-    const noteImageElement = document.querySelector('.notecard-thumbnail');
-    noteImageElement.src = 'assets/warhol-frog.png';
-    console.log(noteImageElement);
-  }
